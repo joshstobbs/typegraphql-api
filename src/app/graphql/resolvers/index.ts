f804d64b1e2b4623fec68428332906df5af84ee8
Josh Stobbs <hello@joshstobbs.com>
2020-03-12 10:20:47 -0400

    I always forget the gitignore

--- src/app/graphql/resolvers/index.ts
+++ src/app/graphql/resolvers/index.ts
@@ -4,12 +4,3 @@ import path from 'path'
 export default glob
 	.sync(`${__dirname}/../../../**/*.resolver.ts`)
 	.map(file => require(path.resolve(file)))
-
-// import fs from 'fs'
-// import path from 'path'
-
-// export default fs.readdirSync(__dirname).map(file => {
-// 	const { name } = path.parse(file)
-
-// 	return require(`./${name}.ts`)
-// })
